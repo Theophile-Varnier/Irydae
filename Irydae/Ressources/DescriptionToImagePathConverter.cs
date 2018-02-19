0d1c7794321559bb089fecebd0c837e46aa3d7a3
Kukukun <theophile.varnier@gmail.com>
2018-02-19 17:05:23 +0100

    jjr

---
+++ Irydae/Ressources/DescriptionToImagePathConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using Irydae.Helpers;
+
+namespace Irydae.Ressources
+{
+    public class DescriptionToImagePathConverter : BaseConverter, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string imgName = "transparent.png";
+            var enumValue = value as Enum;
+            if (enumValue != null)
+            {
+                imgName = string.Format("{0}.png", enumValue.GetDescription());
+            }
+            return Path.Combine("Images", imgName);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

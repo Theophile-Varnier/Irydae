185aaddbbb7072cc5a14897c6e5573d8c6ac0314
Kukukun <theophile.varnier@gmail.com>
2018-04-04 17:03:27 +0200

    ldkls

---
+++ Irydae/Helpers/PartenaireEqualityComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Irydae.Model;
+
+namespace Irydae.Helpers
+{
+    public class PartenaireEqualityComparer : IEqualityComparer<Partenaire>
+    {
+        public bool Equals(Partenaire x, Partenaire y)
+        {
+            return x.Nom == y.Nom;
+        }
+
+        public int GetHashCode(Partenaire obj)
+        {
+            return obj.Nom.GetHashCode();
+        }
+    }
+}
---
+++ Irydae/Model/Personnage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+
+namespace Irydae.Model
+{
+    [JsonObject]
+    public class Personnage
+    {
+        public Personnage()
+        {
+            Periodes = new ObservableCollection<Periode>();
+            Partenaires = new ObservableCollection<Partenaire>();
+        }
+
+        [JsonProperty]
+        public ObservableCollection<Periode> Periodes { get; set; }
+
+        [JsonProperty]
+        public ObservableCollection<Partenaire> Partenaires { get; set; } 
+    }
+}
